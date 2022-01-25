0dfdfbea09efa99009769a129c29c7057921c2ba
shiyi <shiyizhang93@gmail.com>
2022-01-25 11:30:49 +0800

    Finish chapter3 add1.cu

--- chapter3/add1.cu
+++ chapter3/add1.cu
@@ -15,5 +15,61 @@ void check(const double *z, const int N);
 
 int main(void)
 {
-    
+    const int N = 100000000;
+    const int M = sizeof(double) * N;
+    // allocate host memory
+    double *h_x = (double *) malloc(M);
+    double *h_y = (double *) malloc(M);
+    double *h_z = (double *) malloc(M);
+    // initialize host x,y array
+    for (int i = 0; i < N; ++i)
+    {
+        h_x[i] = a;
+        h_y[i] = b;
+    }
+
+    // allocate device memory
+    double *d_x, *d_y, *d_z;
+    cudaMalloc((void **)&d_x, M);
+    cudaMalloc((void **)&d_y, M);
+    cudaMalloc((void **)&d_z, M);
+    // copy data from host to device
+    cudaMemcpy(d_x, h_x, M, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, h_y, M, cudaMemcpyHostToDevice);
+    // set grid size, block size and call add kernel function
+    const int block_size = 128;
+    const int grid_size = N / block_size;
+    add<<<grid_size, block_size>>>(d_x, d_y, d_z);
+    // copy result back from device to host
+    cudaMemcpy(h_z, d_z, M, cudaMemcpyDeviceToHost);
+    check(h_z, N);
+    // release host memory
+    free(h_x);
+    free(h_y);
+    free(h_z);
+    // release device memory
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_z);
+
+    return 0;
+}
+
+void __global__ add(const double *x, const double *y, double *z)
+{
+    const int n = blockDim.x * blockIdx.x + threadIdx.x;
+    z[n] = x[n] + y[n];
+}
+
+void check(const double *z, const int N)
+{
+    bool has_error = false;
+    for (int i = 0; i < N; ++i)
+    {
+        if (fabs(z[i] - c) > EPSILON)
+        {
+            has_error = true;
+        }
+    }
+    printf("%s\n", has_error ? "Has errors" : "No errors");
 }
