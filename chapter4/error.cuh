c8c7ba7386d50928e25749e29b76a0598cab0597
shiyi <shiyizhang93@gmail.com>
2022-03-21 17:36:33 +0800

    Start chapter4 coding

---
+++ chapter4/error.cuh
@@ -0,0 +1,23 @@
+//! \Brief  A CUDA header file containing a CUDA error detecting macro function
+//!
+//! \Author Shiyi Zhang
+//!
+//! \Create 03/21/2022
+
+#pragma once
+#include <stdio.h>
+
+#define CHECK(call)                                                     \
+do                                                                      \
+{                                                                       \
+    const cudaError_t error_code = call;                                \
+    if (error_code != cudaSuccess)                                      \
+    {                                                                   \
+        printf("CUDA Error:\n");                                        \
+        printf("    File:       %s\n", __FILE__);                       \
+        printf("    Line:       %d\n", __LINE__);                       \
+        printf("    Error code: %d\n", error_code);                     \
+        printf("    Error text: %s\n", cudaGetErrorString(error_code);  \
+        exit(1);                                                        \
+    }                                                                   \
+} while (0)
